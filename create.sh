b3e565b71338d6423e6612321c386a2e81075333
cichy2k <42224048+cichy2k@users.noreply.github.com>
2022-08-07 21:23:27 +0200

    Update create.sh

--- create.sh
+++ create.sh
@@ -31,7 +31,7 @@ function error {
 }
 
 # Base raw github URL
-_raw_base="https://raw.githubusercontent.com/ej52/proxmox-scripts/main/lxc/nginx-proxy-manager"
+_raw_base="https://raw.githubusercontent.com/cichy2k/nginx-proxy-manager/main/"
 # Operating system
 _os_type=alpine
 _os_version=3.16
